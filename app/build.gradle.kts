8ecef413f60e9e87abfeb1f156d4e534ffdd8e05
Andriy Litnarovych <andrey.litnarovich.work@gmail.com>
2024-03-14 23:38:30 +0200

    Build gradle dependencies reordering.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
     id("kotlin-kapt")
-    kotlin("kapt")
     id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -52,57 +52,83 @@ android {
     }
 }
 
-dependencies {
-    val room_version = "2.6.1"
-
-    //Core
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-
-    //Tests
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    //Bottom navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
-
-    //MVVM
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-
-    //Generate data for repository
-    implementation("com.github.javafaker:javafaker:1.0.2")
-
-    //Image handling
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-
-    //Statistic diagram
-    implementation("ir.mahozad.android:pie-chart:0.7.0")
-
-    //Indicator dots for ViewPager
-    implementation("com.tbuonomo:dotsindicator:5.0")
-
-    //Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
-
-    //Room
-    implementation("androidx.room:room-runtime:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-
-    //Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
-
-}
-
 kapt {
     correctErrorTypes = true
+}
+
+dependencies {
+    val coreKtxVersion = "1.9.0"
+
+    val legacyVersion = "1.0.0"
+
+    val appCompatVersion = "1.6.1"
+    val fragmentsVersion = "1.6.1"
+    val androidLifecycleVersion = "2.6.1"
+
+    val materialDesignVersion = "1.9.0"
+    val constraintLayoutVersion = "2.1.4"
+    val pieChartVersion = "0.7.0"
+    val viewPagerDotsVersion = "5.0"
+    val glideVersion = "4.12.0"
+
+    val navigationVersion = "2.7.2"
+
+    val hiltVersion = "2.48"
+
+    val coroutineVersion = "1.3.9"
+
+    val javaFakerVersion = "1.0.2"
+
+    val roomVersion = "2.6.1"
+
+    val junitVersion = "4.13.2"
+    val junitExtVersion = "1.1.5"
+    val espressoVersion = "3.5.1"
+
+    //Core
+    implementation("androidx.core:core-ktx:$coreKtxVersion")
+
+    implementation("androidx.legacy:legacy-support-v4:$legacyVersion")
+
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    implementation("androidx.fragment:fragment-ktx:$fragmentsVersion")
+
+    //MVVM
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$androidLifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidLifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$androidLifecycleVersion")
+
+    //View
+    implementation("com.google.android.material:material:$materialDesignVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
+    //Statistic diagram
+    implementation("ir.mahozad.android:pie-chart:$pieChartVersion")
+    //Indicator dots for ViewPager
+    implementation("com.tbuonomo:dotsindicator:$viewPagerDotsVersion")
+    //Image handling
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+
+    //Generate data for repository
+    implementation("com.github.javafaker:javafaker:$javaFakerVersion")
+
+    //Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    //Tests
+    testImplementation("junit:junit:$junitVersion")
+    androidTestImplementation("androidx.test.ext:junit:$junitExtVersion")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVersion")
 }
